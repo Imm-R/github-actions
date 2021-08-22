1e52055c6d00bc924c15764eb635385b1f40810d
Vladimir <Grischcenkov@gmail.com>
2021-08-22 12:24:18 +0000

    add index.html

--- application.py
+++ application.py
@@ -30,7 +30,7 @@ def calc_kvadrat(x):
 
 @application.route("/htmlpage")
 def show_html_page():
-    myfile = open("mypage.html", mode='r')
+    myfile = open("templates/index.html", mode='r')
     page = myfile.read()
     myfile.close()
     return page
